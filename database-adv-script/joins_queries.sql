9e63bd4f488ce63802fb1864362c2e31dc26bd2f
Amos Omariba <amomariba@gmail.com>
2025-05-21 21:48:10 +0300

    Write Complex Queries with Joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -14,12 +14,20 @@ ORDER BY Property.name ASC, Review.rating DESC;
 
 
 -- Used Union to retrieve information of User and Booking
-SELECT * 
-FROM `Booking`
-LEFT JOIN `User`
-ON `Booking`.user_id = `User`.user_id
-FULL OUTER UNION
-SELECT * 
-FROM `Booking`
-RIGHT JOIN `User`
-ON `Booking`.user_id = `User`.user_id;
+-- LEFT JOIN: all bookings + matched users (or NULL)
+SELECT 
+    B.booking_id, B.property_id, B.user_id AS booking_user_id, 
+    B.start_date, B.end_date, B.total_price, B.status, 
+    U.user_id AS user_user_id, U.first_name, U.last_name, U.email
+FROM Booking B
+LEFT JOIN User U ON B.user_id = U.user_id
+
+UNION
+
+-- RIGHT JOIN: all users + matched bookings (or NULL)
+SELECT 
+    B.booking_id, B.property_id, B.user_id AS booking_user_id, 
+    B.start_date, B.end_date, B.total_price, B.status, 
+    U.user_id AS user_user_id, U.first_name, U.last_name, U.email
+FROM Booking B
+RIGHT JOIN User U ON B.user_id = U.user_id;

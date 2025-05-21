cfa7173af3df165b7ad020c86dc25a1702b9a055
Amos Omariba <amomariba@gmail.com>
2025-05-21 21:39:14 +0300

    Write Complex Queries with Joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -9,14 +9,16 @@ ON `Booking`.user_id = `User`.user_id;
 SELECT *
 FROM `Property`
 LEFT JOIN `Review`
-ON `Property`.property_id = `Review`.property_id;
+ON `Property`.property_id = `Review`.property_id
+ORDER BY Property.name ASC, Review.rating DESC;
+
 
 -- Used Union to retrieve information of User and Booking
 SELECT * 
 FROM `Booking`
 LEFT JOIN `User`
 ON `Booking`.user_id = `User`.user_id
-UNION
+FULL OUTER UNION
 SELECT * 
 FROM `Booking`
 RIGHT JOIN `User`
